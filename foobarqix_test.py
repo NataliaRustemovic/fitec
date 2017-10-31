25c449e142d51b4c5b0eb102e16b8196cd623939
NataliaRustemovic <33249840+NataliaRustemovic@users.noreply.github.com>
2017-10-31 14:35:36 +0100

    Test6 1

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -13,7 +13,10 @@ def foobarqix(number):
         result += "Bar"
     if is_divisible(number, 7):
         result += "Qix"
-    return result
+    if result != "":
+        return result
+    else:
+        return "1"
 
 
 class FooBarQixTestCase(unittest.TestCase):
@@ -28,5 +31,8 @@ class FooBarQixTestCase(unittest.TestCase):
 
     def test_return_FooBar_when_number_is_devisible_by_3_and_5(self):
         self.assertEqual(foobarqix(15), "FooBar")
+
+    def test_return_1_when_number_is_1(self):
+        self.assertEqual (foobarqix(1), "1")
 
 unittest.main()
