5fd8d1e146552209ffcaeb82c9a787a4cf8d9f6b
NataliaRustemovic <33249840+NataliaRustemovic@users.noreply.github.com>
2017-10-31 11:50:24 +0100

    Test2

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -1,12 +1,18 @@
 import unittest
 
 def foobarqix(number):
-    return "Foo"
+    if number % 3 == 0:
+        return "Foo"
+    else:
+        return "Bar"
 
 
 class FooBarQixTestCase(unittest.TestCase):
     def test_return_Foo_when_number_is_devisible_by_3(self):
         self.assertEqual(foobarqix(3), "Foo")
+
+    def test_return_Bar_when_number_is_devisible_by_5(self):
+        self.assertEqual(foobarqix(5), "Bar")
 
 
 unittest.main()
